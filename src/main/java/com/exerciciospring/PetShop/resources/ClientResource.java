f86a85f872899c6ac0ed41c507f639a41a9a3de5
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-04-13 12:45:33 -0300

    Entidade Client e ClientResource

---
+++ src/main/java/com/exerciciospring/PetShop/resources/ClientResource.java
@@ -0,0 +1,20 @@
+package com.exerciciospring.PetShop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exerciciospring.PetShop.domain.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@GetMapping
+	public ResponseEntity<Client> findAll() {
+		Client c = new Client(1L, "Lucas Pereira", "lucas@gmail.com", "999999999");
+		return ResponseEntity.ok().body(c);
+	}
+
+}
